9f9be9c018ac40b29a4c3d3683af2bb588832a7c
EMRE <EMRE@DESKTOP-D3VV2GT>
2024-11-08 14:55:09 +0300

    Add project files.

---
+++ ContactsAPI/Model/ContactInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace ContactsAPI.Model
+{
+    public class ContactInfo
+    {
+        public string Type { get; set; }
+
+        public string Content { get; set; }
+    }
+}
---
+++ ContactsAPI/Model/Person.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.Runtime.Serialization;
+
+namespace ContactsAPI.Model
+{
+    public class Person
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Company { get; set; }
+
+        public List<ContactInfo> ContactInfos { get; set; }
+
+    }
+}
